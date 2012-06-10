44254bcc31a941cba6607045d341d38be20d55d7
Parantapa Bhattacharya <pb@parantapa.net>
2012-06-10 13:50:37 +0200

    Faster merge by low level manipulation

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ from numpy import get_include
 ext_modules = [
     Extension("staticgraph.make_digraph", ["staticgraph/make_digraph.pyx"],
               include_dirs=[get_include()]),
+    Extension("staticgraph.manip_digraph", ["staticgraph/manip_digraph.pyx"],
+              include_dirs=[get_include()]),
     Extension("staticgraph.cGraph", ["staticgraph/cGraph.pyx"],
               include_dirs=[get_include()])
     ]
@@ -13,7 +15,7 @@ ext_modules = [
 packages = ["staticgraph", "staticgraph.algorithms"]
 
 setup(name = "StaticGraph",
-      version = "0.53a",
+      version = "0.54a",
       packages = packages,
       ext_modules = ext_modules,
       cmdclass = {'build_ext': build_ext}
