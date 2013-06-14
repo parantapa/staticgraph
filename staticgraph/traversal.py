f6d60bf8f7601fa2562bd7d36ab7716caf8fccfd
Arpan Das <arpandas91@gmail.com>
2013-06-14 11:26:39 +0530

    Tracking Predecessor nodes for traversals added.
    Two dimensional numpy array returned, 1 for the nodes reached, another for the predecessors

--- staticgraph/traversal.py
+++ staticgraph/traversal.py
@@ -8,8 +8,8 @@ from staticgraph.exceptions import StaticGraphNodeAbsentException
 
 def traverse_bfs(G, s):
     """
-    Returns a sequence of vertices of undirected staticgraph G 
-    in a breadth-first-search order starting at source s.
+    Returns a sequence of vertices alongwith their predecessors for 
+    an undirected staticgraph G in a breadth-first-search order starting at source s.
     
     Parameters
     ----------
@@ -34,33 +34,36 @@ def traverse_bfs(G, s):
     visited[order : order * 2] = 0
     visited = visited.reshape(2, order)
     
-    queue = zeros((order), dtype = int32)
-    res = zeros((order), dtype = int32)
+    queue = zeros((2, order), dtype = int32)
+    res = zeros((2, order), dtype = int32)
     
     front = rear = 0
-    queue[rear] = s
+    queue[0, rear] = s
+    queue[1, rear] = -1
     rear = 1
     visited[1, s] = 1
     index = 0
     
     while (front != rear):
-        u = queue[front]
-        front = front + 1
-        res[index] = u
+        u = queue[0, front]
+        res[0, index] = u
+        res[1, index] = queue[1, front]
         index = index + 1
+        front = front + 1
         start = G.n_indptr[u]
         stop  = G.n_indptr[u + 1]
         for v in imap(int, G.n_indices[start:stop]):
             if visited[1, v] == 0:
                 visited[1, v] = 1
-                queue[rear] = v
+                queue[0, rear] = v
+                queue[1, rear] = u
                 rear = rear + 1
     return res
 
 def traverse_dfs(G, s):
     """
-    Returns a sequence of vertices of undirected staticgraph G 
-    in a depth-first-search order starting at source s.
+    Returns a sequence of vertices alongwith their predecessors for 
+    an undirected staticgraph G in a depth-first-search order starting at source s.
 
     Parameters
     ----------
@@ -85,14 +88,15 @@ def traverse_dfs(G, s):
     visited[order : order * 2] = 0
     visited = visited.reshape(2, order)
     
-    res = zeros((order), dtype = int32)
-    stack = zeros((order), dtype = int32)
+    res = zeros((2, order), dtype = int32)
+    stack = zeros((2, order), dtype = int32)
 
     visited[1, s] = 1
     index = 1
     top = 0
-    stack[top] = s
-    res[0] = s
+    res[0, 0] = stack[0, top] = s
+    res[1, 0] = stack[1, top] = -1
+    
     while top >= 0:
         u = stack[top]
         start = G.n_indptr[u]
@@ -101,8 +105,8 @@ def traverse_dfs(G, s):
             if visited[1, v] == 0:
                 visited[1, v] = 1
                 top = top + 1
-                stack[top] = v
-                res[index] = v
+                res[0, index] = stack[0, top] = v
+                res[1, index] = stack[1, top] = u
                 index = index + 1
                 break
         else:
