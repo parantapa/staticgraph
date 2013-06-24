7cf46a8229e3ae9766d28e15f8a037577b72e8fe
Arpan Das <arpandas91@gmail.com>
2013-06-24 20:24:11 +0530

    Bugs in dijkstra algorithm implementation removed.
    Relevant Docstrings added.

--- staticgraph/wgraph_dijkstra.py
+++ staticgraph/wgraph_dijkstra.py
@@ -1,43 +1,61 @@
 """
-Module implementing the standard shortest path algorithms for unweighted graphs
+Module implementing the Dijkstra shortest path algorithm for undirected graphs
 """
 
 from numpy import arange, uint32, zeros, empty, float64, uint8
-from itertools import imap
 from staticgraph.exceptions import StaticGraphNodeAbsentException
-from math import floor
 
 def isEmpty(prior_queue):
+    """
+    Returns whether the priority queue is empty or not.
+    """
+
     return prior_queue[0] == (2 ** 32) - 1
 
 def min_heapify(heap, weights, i, heap_size):
-    left = 2 * i
-    right = (2 * i) + 1
+    """
+    Module that maintains heap property of the heap.
+    """
+
+    left = (2 * i) + 1
+    right = (2 * i) + 2
     smallest = i
-    if left < heap_size and weights[heap[left - 1]] < weights[heap[i - 1]]:
+    if left < heap_size and weights[heap[left]] < weights[heap[i]]:
         smallest = left
-    if right < heap_size and weights[heap[right - 1]] < weights[heap[i - 1]]:
+    if right < heap_size and weights[heap[right]] < weights[heap[smallest]]:
         smallest = right
         
     if smallest != i:
-        heap[i - 1], heap[smallest - 1] = heap[smallest - 1], heap[i - 1]
+
+        #exchanging the values in the heap
+        heap[i], heap[smallest] = heap[smallest], heap[i]
         min_heapify(heap, weights, smallest, heap_size)
 
 def build_min_heap(p_queue, weights):
+    """
+    Module to create a heap from an unsorted array p_queue
+    according to the corresponding values of weights.
+    """
+
     heap_size = p_queue.size
-    index = int(floor(p_queue.size / 2))
-    for i in range(index, 1):
+    for i in range(p_queue.size // 2, -1, -1):
         min_heapify(p_queue, weights, i, heap_size)
 
 def extract_min_dist(p_queue, weights, heap_size):
+    """
+    Module to extract the minimum element from the heap
+    """
+
     minimum = p_queue[0]
     p_queue[0] = p_queue[heap_size - 1]
     p_queue[heap_size - 1] = (2 ** 32) - 1
     heap_size -= 1
-    min_heapify(p_queue, weights, 1, heap_size)
+    
+    #Restoring heap property
+    min_heapify(p_queue, weights, 0, heap_size)
     return minimum
 
-def dijkstra_all(G, s, maxdepth = 2 ** 32 - 1):
+def dijkstra_all(G, s):
     """
     Returns a sequence of vertices alongwith the length of their 
     shortest paths from source node s for an undirected 
@@ -52,17 +70,20 @@ def dijkstra_all(G, s, maxdepth = 2 ** 32 - 1):
     
     Returns
     -------
-    path : A 2-dimensional numpy int32 array.
-          1st row: The set of vertices.
-          2nd row: Corresponding predecessors in shortest path from the source
-          3rd row: Corresponding distances from the source in the shortest path
+    path : 2 numpy arrays.
 
+           nodes : A numpy uint32 array containing the sequence of vertices 
+                   sorted according to distance from the source.
+
+           weights : A numpy float64 array containing the corresponding 
+                     cumulative weights in the shortest path from the source.
+    
+    
     Notes
     ------
 
     It is mandatory that G be undirected.
-    Precessor is -1 for the source node.
-    distance = -1 implies that the node is unreachable from the source.
+    weights = (2 ** 64) - 1implies that the node is unreachable from the source.
     """
     if s >= G.order():
         raise StaticGraphNodeAbsentException("source node absent in graph!!")
@@ -71,10 +92,9 @@ def dijkstra_all(G, s, maxdepth = 2 ** 32 - 1):
     nodes = arange(order, dtype = uint32)
     weights = empty(order, dtype = float64)
     visited = zeros(order, dtype = uint8)
-    weights[:] = 2 ** 32 -1
+    weights[:] = (2 ** 64) -1
     weights[s] = 0
     build_min_heap(nodes, weights)
-    
     heap_size = order
     while isEmpty(nodes) == False:
         u = extract_min_dist(nodes, weights, heap_size)
@@ -84,7 +104,7 @@ def dijkstra_all(G, s, maxdepth = 2 ** 32 - 1):
             if visited[v] == 0:
                 if weights[v] > (weights[u] + G.weight(u, v)):
                     weights[v] = (weights[u] + G.weight(u, v))
-        nodes = arange(order, dtype = uint32)
+    nodes = arange(order, dtype = uint32)
     sort_indices = weights.argsort()
     weights = weights[sort_indices]
     nodes = nodes[sort_indices]
